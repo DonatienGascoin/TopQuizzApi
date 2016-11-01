caceb4722b84de41889d19f71705d0d8ffc958d0
Donatien <donatien.gascoin@gmail.com>
2016-11-01 11:15:14 +0100

    First commit
    Project creation

---
+++ src/main/java/com/quizz/database/AppBoot.java
@@ -0,0 +1,17 @@
+package com.quizz.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class AppBoot 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AppBoot.class, args);
+    }
+}
