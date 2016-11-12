046f8652ecf0b40303cb5b6b06682fba87c2e2ae
Maxence Royer <maxence.royer@worldline.com>
2016-11-12 18:09:44 +0100

    [MRR][0.1/startQuizz] Add getAllQuizzesByPseudo && getQuizzByName methods

--- src/main/java/com/quizz/database/services/QuizzService.java
+++ src/main/java/com/quizz/database/services/QuizzService.java
@@ -1,5 +1,19 @@
 package com.quizz.database.services;
 
+import com.quizz.database.modeles.ReturnObject;
+
+/**
+ * 
+ * {@link QuizzService} will validate data and call {@link QuizzRepository} 
+ * 
+ * @author Maxence Royer
+ * @version 0.1
+ * @since 12/11/2016
+ * 
+ */
 public interface QuizzService {
 
+	public ReturnObject getAllQuizzesByPseudo(String pseudo);
+
+	public ReturnObject getQuizzByName(String name);
 }
